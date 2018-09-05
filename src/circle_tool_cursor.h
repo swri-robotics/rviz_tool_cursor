26a430217fc2611e357791709f9d381033cfc83b
mripperger <michael.ripperger@swri.org>
2018-09-05 11:49:56 -0500

    Added implementation for a tool cursor that is a line circle

---
+++ src/circle_tool_cursor.h
@@ -0,0 +1,31 @@
+#ifndef RVIZ_TOOL_CURSOR_CIRCLE_TOOL_CURSOR_H
+#define RVIZ_TOOL_CURSOR_CIRCLE_TOOL_CURSOR_H
+
+#include <rviz_tool_cursor/rviz_tool_cursor.h>
+
+namespace rviz
+{
+  class FloatProperty;
+}
+
+namespace rviz_tool_cursor
+{
+
+class CircleToolCursor : public ToolCursor
+{
+Q_OBJECT
+public:
+
+  CircleToolCursor();
+
+protected:
+
+  virtual Ogre::ManualObject* createToolVisualization() override;
+
+  rviz::FloatProperty* radius_property_;
+
+};
+
+} // namespace rviz_tool_cursor
+
+#endif // RVIZ_TOOL_CURSOR_CIRCLE_TOOL_CURSOR_H
